07ba5ffcabdd6d0e668395ed12e9a6ec324ec6d3
Leo <Leooonard@163.com>
2015-11-13 13:16:02 +0800

    添加两个converter

---
+++ Intersect/Converter/LabelIDToComboBoxItemNotVisbilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Intersect
+{
+    public class LabelIDToComboBoxItemNotVisbilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int labelID = (int)value;
+            Label label = new Label();
+            label.id = labelID;
+            label.select();
+            if (label.isRaster)
+            {
+                return "Visible";
+            }
+            else
+            {
+                return "Collapsed";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Intersect/Converter/LabelIDToComboBoxItemVisbilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Intersect
+{
+    public class LabelIDToComboBoxItemVisbilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int labelID = (int)value;
+            Label label = new Label();
+            label.id = labelID;
+            label.select();
+            if (label.isRaster)
+            {
+                return "Collapsed";
+            }
+            else
+            {
+                return "Visible";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
